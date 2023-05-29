ee9ae35c76a05109880030ab07a16587999ed03a
Misaki.Fujishiro <Misaki.Fujishiro@nttdata.com>
2023-05-29 15:50:35 +0900

    sqs consumer enable

--- src/main/java/com/msa/aws/sqs/sqs_consumer/MessageReceiver.java
+++ src/main/java/com/msa/aws/sqs/sqs_consumer/MessageReceiver.java
@@ -15,24 +15,24 @@ public class MessageReceiver {
 
     public void receiveMessage(){
     String url = "https://sqs.ap-northeast-1.amazonaws.com/626394096352/MA-fujishiroms-sqs-standard";
-//
-//    ReceiveMessageRequest request = new ReceiveMessageRequest()
-//                .withQueueUrl(url)
-//                .withWaitTimeSeconds(5)
-//                .withMaxNumberOfMessages(5);
-//
-//    ReceiveMessageResult result = amazonSQSClient.receiveMessage(request);
-//
-//    for (Message msg : result.getMessages()) {
-//        // 受信したメッセージの情報を表示
-//        System.out.println("["+msg.getMessageId()+"]");
-//        System.out.println("  Message ID     : " + msg.getMessageId());
-//        System.out.println("  Receipt Handle : " + msg.getReceiptHandle());
-//        System.out.println("  Message Body   : " + msg.getBody());
-//        System.out.println();
-//
-//        // 受信したメッセージを削除
-//        amazonSQSClient.deleteMessage(url, msg.getReceiptHandle());
-//        }
+
+    ReceiveMessageRequest request = new ReceiveMessageRequest()
+                .withQueueUrl(url)
+                .withWaitTimeSeconds(5)
+                .withMaxNumberOfMessages(5);
+
+    ReceiveMessageResult result = amazonSQSClient.receiveMessage(request);
+
+    for (Message msg : result.getMessages()) {
+        // 受信したメッセージの情報を表示
+        System.out.println("["+msg.getMessageId()+"]");
+        System.out.println("  Message ID     : " + msg.getMessageId());
+        System.out.println("  Receipt Handle : " + msg.getReceiptHandle());
+        System.out.println("  Message Body   : " + msg.getBody());
+        System.out.println();
+
+        // 受信したメッセージを削除
+        amazonSQSClient.deleteMessage(url, msg.getReceiptHandle());
+        }
     }
 }
