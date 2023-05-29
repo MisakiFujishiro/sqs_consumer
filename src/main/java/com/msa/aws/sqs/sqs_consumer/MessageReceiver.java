56d0e45a4847f9d2ebddb575f9644ca07436b8f7
Misaki.Fujishiro <Misaki.Fujishiro@nttdata.com>
2023-05-29 17:11:16 +0900

    wait time check doing and polling

--- src/main/java/com/msa/aws/sqs/sqs_consumer/MessageReceiver.java
+++ src/main/java/com/msa/aws/sqs/sqs_consumer/MessageReceiver.java
@@ -5,6 +5,7 @@ import com.amazonaws.services.sqs.model.Message;
 import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
 import com.amazonaws.services.sqs.model.ReceiveMessageResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -15,46 +16,59 @@ public class MessageReceiver {
     @Autowired
     private AmazonSQS amazonSQSClient;
 
+    // check processing or waiting
+    private boolean processing=false;
 
+    @Scheduled(fixedDelay = 1000)
     public void receiveMessage(){
-    String url = "https://sqs.ap-northeast-1.amazonaws.com/626394096352/MA-fujishiroms-sqs-standard";
+        if(processing){
+            return;
+        }
 
-    ReceiveMessageRequest request = new ReceiveMessageRequest()
-                .withQueueUrl(url)
-                .withWaitTimeSeconds(5)
-                .withMaxNumberOfMessages(5);
+        String url = "https://sqs.ap-northeast-1.amazonaws.com/626394096352/MA-fujishiroms-sqs-standard";
 
-    ReceiveMessageResult result = amazonSQSClient.receiveMessage(request);
+        ReceiveMessageRequest request = new ReceiveMessageRequest()
+                    .withQueueUrl(url)
+                    .withWaitTimeSeconds(5)
+                    .withMaxNumberOfMessages(5);
 
-
-    System.out.println(result);
-    for (Message msg : result.getMessages()) {
+        ReceiveMessageResult result = amazonSQSClient.receiveMessage(request);
 
 
-        // 受信したメッセージの情報を表示
+
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        LocalDateTime now_bf = LocalDateTime.now();
-        System.out.println(" Recived Time: "+ formatter.format(now_bf));
 
 
-        // 受信したメッセージの情報を表示
-        System.out.println("["+msg.getMessageId()+"]");
-        System.out.println("  Message ID     : " + msg.getMessageId());
-        System.out.println("  Receipt Handle : " + msg.getReceiptHandle());
-        System.out.println("  Message Body   : " + msg.getBody());
+
+        for (Message msg : result.getMessages()) {
+            processing=true;
+            // 処理開始時間
+            LocalDateTime now_bf = LocalDateTime.now();
+            System.out.println(" Recived Time: "+ formatter.format(now_bf));
 
 
-        // 受け取った数字分だけ待機をする
-        int waitTime = Integer.parseInt(msg.getBody())*1000;
-        waitInMilliseconds(waitTime);
-        LocalDateTime now_af = LocalDateTime.now();
-        System.out.println(" Recived Time: "+ formatter.format(now_af));
+            // 受信したメッセージの情報を表示
+            System.out.println("["+msg.getMessageId()+"]");
+            System.out.println("  Message ID     : " + msg.getMessageId());
+            System.out.println("  Receipt Handle : " + msg.getReceiptHandle());
+            System.out.println("  Message Body   : " + msg.getBody());
 
 
-        System.out.println();
+            // 受け取った数字分だけ待機をする
+            int waitTime = Integer.parseInt(msg.getBody())*1000;
+            System.out.println(waitTime);
+            waitInMilliseconds(waitTime);
 
-        // 受信したメッセージを削除
-        amazonSQSClient.deleteMessage(url, msg.getReceiptHandle());
+            // 処理終了時間
+            LocalDateTime now_af = LocalDateTime.now();
+            System.out.println(" Recived Time: "+ formatter.format(now_af));
+
+
+
+            // 受信したメッセージを削除
+            amazonSQSClient.deleteMessage(url, msg.getReceiptHandle());
+
+            processing=false;
         }
     }
     private void waitInMilliseconds(int milliseconds) {
